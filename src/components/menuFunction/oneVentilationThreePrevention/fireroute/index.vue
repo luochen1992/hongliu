aefc47a0da4d657133c0957ccf837b0c76bcebe7
lwj <363072973@qq.com>
2021-11-29 16:29:26 +0800

    将避灾路线放到sdk 中，直接调用new CTMap.Flow 即可

--- src/components/menuFunction/oneVentilationThreePrevention/fireroute/index.vue
+++ src/components/menuFunction/oneVentilationThreePrevention/fireroute/index.vue
@@ -4,7 +4,6 @@
 
 <script>
 import { mapGetters, mapMutations } from 'vuex'
-import Flow from '../../../../js/Flow'
 export default {
   name: 'fireroute',
   computed: {
@@ -37,23 +36,19 @@ export default {
     }),
     // 显示
     show() {
-      const waterflow = new Flow(window.viewer, this.rescueType, this.rescueDirect)
+      window.waterflow = new CTMap.Flow(window.viewer, this.rescueType, this.rescueDirect)
        // 生成避灾路线
       const width = 6 // 巷道宽度
       const height = 4 // 巷道高度
       const speed = 0.1 // 移动速度
       const imageFile = 'img/avoidingDisaster/27.png' // 箭头图片
       const arrowDis = 200.0 // 每多少米一个箭头
-      waterflow.getNeededRoute(window.g_hangdao, width, height, speed, imageFile, arrowDis)
+      window.waterflow.getNeededRoute(window.g_hangdao, width, height, speed, imageFile, arrowDis)
     },
     // 隐藏
     hide() {
-       if (window.cesiumvariate[this.rescueType]) {
-        for (let i = 0; i < window.cesiumvariate[this.rescueType].length; i++) {
-          viewer.dataSources.remove(window.cesiumvariate[this.rescueType][i])
-        }
-        window.cesiumvariate[this.rescueType] = []
-      }
+       window.waterflow.remove();
+       window.waterflow = null
     }
 
   }
